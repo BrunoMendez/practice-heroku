3d2fde4e5750e7e4dbacd02b423560e4b3c86485
BrunoMendez <bruno.m.275@gmail.com>
2019-11-14 08:17:26 -0600

    first commit

---
+++ model.js
@@ -0,0 +1,76 @@
+let mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+let studentSchema = mongoose.Schema({
+	firstName : { type : String },
+	lastName : { type : String },
+	id : { 
+			type : Number,
+			required : true }
+});
+
+let userSchema = mongoose.Schema({
+	username : {type : String},
+	password : {type : String}
+});
+
+let User = mongoose.model('User', userSchema);
+let Student = mongoose.model( 'Student', studentSchema );
+
+
+let StudentList = {
+	get : function(){
+		return Student.find()
+				.then( students => {
+					return students;
+				})
+				.catch( error => {
+					throw Error( error );
+				});
+	},
+	getByID : function(id){
+		return Student.findOne({id : id})
+			.then(student => {
+				return student;
+			})
+			.catch( error => {
+				throw Error( error );
+			});
+
+	},
+	post : function( newStudent ){
+		return Student.create( newStudent )
+				.then( student => {
+					return student;
+				})
+				.catch( error => {
+					throw Error(error);
+				});
+	},
+	put : function( updatedStudent ){
+		return StudentList.getByID( updatedStudent.id )
+			.then( student => {
+				if ( student ){
+					return Student.findOneAndUpdate( {id : student.id}, {$set : updatedStudent}, {new : true})
+						.then( newStudent => {
+							return newStudent;
+						})
+						.catch(error => {
+							throw Error(error);
+						});
+				}
+				else{
+					throw Error( "404" );
+				}
+			})
+			.catch( error => {
+				throw Error(error);
+			});
+	}
+};
+
+
+module.exports = { StudentList };
+
+
