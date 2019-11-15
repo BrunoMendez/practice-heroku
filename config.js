323be3b63d746a3148f4f50e5d01419927c3176d
BrunoMendez <bruno.m.275@gmail.com>
2019-11-14 21:06:14 -0600

    Fourth

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 
-exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb://localhost/studentsDB";
+exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb+srv://bruno:Kal-el97@cluster0-iidfe.mongodb.net/studentsDB?retryWrites=true&w=majority";
 exports.PORT = process.env.PORT || 8080;
